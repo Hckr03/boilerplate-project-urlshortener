6fe69b9f6efc30ef34ca321ad81c790e1b73a87a
Hckr03 <gabu.1991@gmail.com>
2025-09-14 17:40:24 -0300

    practica url shortener microservice

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+//mis librerias
+const bodyParser = require('body-parser');
+const dns = require('dns');
+const urlParser = require('url');
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -14,9 +19,39 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+//mis midlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+//mis cambios
+const urlDatabase = [];
+let short_url_counter = 1;
+
+app.post('/api/shorturl', (req, res) => {
+  const urlOriginal = req.body.url;
+  const hostname = urlParser.parse(urlOriginal).hostname;
+
+  dns.lookup(hostname, (err, address) => {
+    if (err || !address) return res.json({ error: 'invalid url' });
+
+    const short_url = short_url_counter++;
+    urlDatabase.push({ original_url: urlOriginal, short_url });
+    
+    res.json({ original_url: urlOriginal, short_url });
+  });
+});
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const short_url = parseInt(req.params.short_url);
+  const found = urlDatabase.find(entry => entry.short_url === short_url);
+
+  if (found) res.redirect(found.original_url);
+  else res.json({ error: 'No short URL found for the given input' });
 });
 
 app.listen(port, function() {
